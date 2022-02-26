4105958031e33678ea8e226984073b545e0218d5
Milin QB <milin@qburst.com>
2022-02-26 17:07:20 +0530

    Updates

---
+++ precache-manifest.ab16133d6c23f162cc38564b6e7aebb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06698064ecb5d791379f86df83758eaa",
+    "url": "/monsters-rolodex-complete/index.html"
+  },
+  {
+    "revision": "4df794d696d7f985b509",
+    "url": "/monsters-rolodex-complete/static/css/main.f251e27e.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex-complete/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex-complete/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4df794d696d7f985b509",
+    "url": "/monsters-rolodex-complete/static/js/main.48c245b7.chunk.js"
+  },
+  {
+    "revision": "4b601965c2149392e1f2",
+    "url": "/monsters-rolodex-complete/static/js/runtime-main.b3c2bfd0.js"
+  }
+]);
